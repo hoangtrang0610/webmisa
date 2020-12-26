68fae7dcf5e3d7515c2a90d94ec347bbbc929b09
hoangtrang0610 <69054102+hoangtrang0610@users.noreply.github.com>
2020-12-26 12:01:03 +0700

    customer

--- js/common/common.js
+++ js/common/common.js
@@ -1,1 +1,37 @@
-﻿
+﻿/**---------------
+ * format dữ liệu ngày tháng sang ngày/tháng/năm
+ * @param {any} date tham số kiểu dữ liệu bất kì
+ */
+
+function formatDate(date) {
+    date = new Date(date);
+    if (Number.isNaN(date.getTime())) {
+        return "";
+    } else {
+        var day = date.getDate(),
+            month = date.getMonth() + 1,
+            year = date.getFullYear();
+
+        day = day < 10 ? '0' + day : day;
+        month = day < 10 ? '0' + month : month;
+
+        return day + '/' + month + '/' + year;
+    }
+
+}
+
+/**---------------
+ * format lương
+ * @param {number} money kiểu number
+ */
+
+function formatMoney(money) {
+    if (money !== null) {
+        var num = money.toFixed(0).replace(/(\d)(?=(\d\d\d)+(?!\d))/g, "$1.");
+        return num;
+    }
+    else {
+        return "";
+    }
+
+}
--- js/page/employee.js
+++ js/page/employee.js
@@ -12,19 +12,26 @@ function loadData() {
         var data = res;
        
         $.each(data, function (index, item) {
-            debugger;
-       
+            
+            var DateOfBirth = item["DateOfBirth"];
+            var salary = item["Salary"];
+            salary = formatMoney(salary);
+            DateOfBirth = formatDate(DateOfBirth);
+            var checkbox = `<input type="checkbox"/>`;
+            if (item.GenderName == "Nam") {
+                var checkbox = `<input type="checkbox" checked/>`;
+            }
             var tr = `<tr>
                         <td><div><span>`+ item.EmployeeCode + `</span></div></td>
                         <td><div><span>`+ item['FullName'] + `</span></div></td>
-                        <td><div><span>`+ item.GenderName + `</span></div></td>
-                        <td><div><span>`+ item.DateOfBirth + `</span></div></td>
+                        <td><div class="text-align-center">`+ checkbox + `</div></td>
+                        <td><div><span>`+ DateOfBirth + `</span></div></td>
                         <td><div><span>`+ item.PhoneNumber + `</span></div></td>
                         <td><div><span>`+ item.Email + `</span></div></td>
                         <td><div><span>`+ item.PositionName + `</span></div></td>
                         <td><div><span>`+ item.DepartmentName + `</span></div></td>
-                        <td><div><span>`+ item.Salary + `</span></div></td>
-                        <td><div style="max-width:250px"><span>`+ item.Address + `</span></div></td>
+                        <td><div class="text-align-right">`+ salary + `</div></td>
+                        <td style="max-width:150px"  title="`+ item.Address +`">` + item.Address + `</td>
                         <td><div><span>`+ item.WorkStatusName + `</span></div></td>
                     </tr>`;
             $('table tbody').append(tr);
@@ -36,3 +43,4 @@ function loadData() {
     }).fail(function (res) { })
     //binding dữ liệu lên table:
 }
+
